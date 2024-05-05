4fbb88e0ef634dcd9afc81b8180228a4ee2f2eaa
Stuart <97462203+Stu-123@users.noreply.github.com>
2024-05-05 22:03:29 +0100

    Added user input via command line interface.

--- main.py
+++ main.py
@@ -69,10 +69,24 @@ class PDFReport:
         webbrowser.open(self.filename)
 
 
-# Testing
-my_bill = Bill(amount=120, period="March 2024")
-Euan = Flatmate(name="Euan", days_in_house=20)
-Sol = Flatmate(name="Sol", days_in_house=30)
+# Gathering user input via command line interface
+bill_amount = float(input("Hello, please enter the bill amount: "))
+bill_period = input("Please enter the bill period: ")
+
+name_1 = input("What is your name? ")
+days_in_house_1 = int(input(f"How many days did {name_1} stay in the house?"))
+
+name_2 = input("What is your flatmate's name? ")
+days_in_house_2 = int(input(f"How many days did {name_2} stay in the house?"))
+
+my_bill = Bill(bill_amount, bill_period)
+flatmate_1 = Flatmate(name=name_1, days_in_house=days_in_house_1)
+flatmate_2 = Flatmate(name=name_2, days_in_house=days_in_house_2)
+
+print(f"{name_1} pays: £", round(flatmate_1.pays(bill=my_bill, other_flatmate=flatmate_2), ndigits=2))
+print(f"{name_2} pays: £", round(flatmate_2.pays(bill=my_bill, other_flatmate=flatmate_1), ndigits=2))
 
 pdf_report = PDFReport(filename="Test_Report.pdf")
-pdf_report.generate(flatmate_1=Euan, flatmate_2=Sol, bill=my_bill)
+pdf_report.generate(flatmate_1, flatmate_2, bill=my_bill)
+
+print("Your PDF bill report has been generated.")
